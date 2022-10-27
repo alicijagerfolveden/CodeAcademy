e60ec15cde50107dfdb78365a7e4d663fd7501ff
Alicija Gerfolveden <alicija.gerf@gmail.com>
2022-10-27 08:09:39 +0300

    25 lesson

--- JavaScript/25diena/23lesson/main.js
+++ JavaScript/25diena/23lesson/main.js
@@ -1,23 +1,14 @@
-const getCars = async () => {
-  try {
-    const response = await fetch("https://robust-safe-crafter.glitch.me/");
+import { createTable } from "./modules/createTable.js";
+import { getCars } from "./modules/getCars.js";
+// import { deleteCar } from "./modules/deleteCar.js";
 
-    const cars = await response.json();
+const cars = await getCars();
 
-    console.log(cars);
-    return cars;
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const onCarDelete = (deletedId) => {
+//   console.log(products);
+//   products = products.filter((product) => product.id !== deletedId);
 
-getCars();
+//   renderProducts(products);
+// };
 
-// try {
-//     const response = await fetch(
-//       `https://jsonplaceholder.typicode.com/posts/${postNumber}`,
-//       { method: "GET" } // šio objekto nebūtina pridėti, GET yra default
-//     );
-//     const post = await response.json();
-
-//     return post;
+createTable(cars);
---
+++ JavaScript/25diena/23lesson/modules/deleteCar.js
@@ -0,0 +1,20 @@
+import { addRow } from "./addRow.js";
+
+// const deleteCar = async () => {
+//   try {
+//     const response = await fetch("https://olive-bead-glazer.glitch.me", {
+//       method: "DELETE",
+//     });
+
+//     const isCarDeleted = response.ok;
+
+//     if (isCarDeleted) {
+//       onCarDelete(idToDelete);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+//   console.log("suveikė");
+// };
+
+// export { deleteCar };
