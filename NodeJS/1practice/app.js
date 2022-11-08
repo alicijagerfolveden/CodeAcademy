3a6135e757a26fe53ef44fde79fdc5fc0333adeb
Alicija Gerfolveden <alicija.gerf@gmail.com>
2022-11-08 22:01:21 +0200

    first commit for nodejs

---
+++ NodeJS/1practice/app.js
@@ -0,0 +1,25 @@
+const casual = require("casual");
+
+const city = casual.city;
+
+console.log(city);
+
+const getRandomNumber = () => {
+  const number = Math.round(Math.random() * 10);
+  return number;
+};
+
+getRandomNumber();
+
+const suffix = casual.suffix[getRandomNumber()];
+const firstName = casual.first_name;
+const lastName = casual.last_name;
+
+console.log(suffix);
+console.log(`${suffix} ${firstName} ${lastName}`);
+
+// const sayHello = (name) => {
+//   console.log(`Hello world and ${name}`);
+// };
+
+// sayHello("Alicija");
