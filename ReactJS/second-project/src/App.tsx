b0cf2a61a9a8ece7a4852fac97e7539f6177e6a9
Alicija Gerfolveden <alicija.gerf@gmail.com>
2023-01-25 19:53:31 +0200

    lesson practice

--- ReactJS/second-project/src/App.tsx
+++ ReactJS/second-project/src/App.tsx
@@ -1,17 +1,24 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Form } from "./components/Form";
+import { NoteForm } from "./components/NoteForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Labas</p>
+        <p>Studentai</p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
 
       <main>
-        <Form />
+        {/* <Form /> */}
+
+        <NoteForm>
+          <h1>Notes App</h1>
+          <h2>Welcome</h2>
+        </NoteForm>
       </main>
     </div>
   );
